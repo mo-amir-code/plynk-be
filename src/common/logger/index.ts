716a5d5bdd3dbbbbc17fc0ac17d1119ad08b91f4
Shubham071122 <shubhamkumar7112002@gmail.com>
2026-04-03 10:26:19 +0530

    chore: remove generated Prisma client files and update gitignore and update the loggging for dev.

--- src/common/logger/index.ts
+++ src/common/logger/index.ts
@@ -1,7 +1,25 @@
 import pino from "pino";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const logger = pino({
   level: process.env.LOG_LEVEL || "info",
+  formatters: {
+    level: (label) => {
+      return { level: label.toUpperCase() };
+    },
+  },
+  transport: isProduction
+    ? undefined
+    : {
+        target: "pino-pretty",
+        options: {
+          colorize: true,
+          translateTime: "SYS:standard",
+          ignore: "pid,hostname",
+          // singleLine: true,
+        },
+      },
 });
 
 export default logger;
