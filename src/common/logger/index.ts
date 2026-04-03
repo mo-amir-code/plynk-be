1f938f1b752188859c624439dac62e2239fdaa48
Shubham071122 <shubhamkumar7112002@gmail.com>
2026-04-03 12:05:56 +0530

    feat: disable base fields in pino logger configuration

--- src/common/logger/index.ts
+++ src/common/logger/index.ts
@@ -4,6 +4,7 @@ const isProduction = process.env.NODE_ENV === "production";
 
 const logger = pino({
   level: process.env.LOG_LEVEL || "info",
+  base: undefined,
   formatters: {
     level: (label) => {
       return { level: label.toUpperCase() };
